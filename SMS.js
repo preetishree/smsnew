ffca61f0f028d0d06ed63af42a1b97d7003e1ce8
preetishree <preetishree@krititech.in>
2013-04-30 00:55:37 +0530

    Update SMS.js

--- SMS.js
+++ SMS.js
@@ -1,9 +1,9 @@
 var SMS = function() {};
 
 SMS.prototype.send = function(phoneNumber,message,successCallback,failureCallback) {
-    return PhoneGap.exec( successCallback, failureCallback, 'SMSPlugin', 'send', [phoneNumber, message]);
+    return cordova.exec( successCallback, failureCallback, 'SMSPlugin', 'send', [phoneNumber, message]);
 };
 
-PhoneGap.addConstructor(function() {
-    PhoneGap.addPlugin("SMSPlugin", new SMS());
+cordova.addConstructor(function() {
+    cordova.addPlugin("SMSPlugin", new SMS());
 });
